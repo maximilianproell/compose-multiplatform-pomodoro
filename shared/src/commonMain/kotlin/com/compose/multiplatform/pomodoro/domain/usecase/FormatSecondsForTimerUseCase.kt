a2f8b5cc094d4eff12e2fd46a2b52238ced4fef7
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-11-16 14:07:39 +0100

    - Fixed shadowing of `seconds` variable in FormatSecondsForTimerUseCase

--- shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/domain/usecase/FormatSecondsForTimerUseCase.kt
+++ shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/domain/usecase/FormatSecondsForTimerUseCase.kt
@@ -2,9 +2,9 @@ package com.compose.multiplatform.pomodoro.domain.usecase
 
 object FormatSecondsForTimerUseCase {
     operator fun invoke(seconds: Int): String {
-        val minutes = (seconds / 60).toString().padStart(length = 2, padChar = '0')
-        val seconds = (seconds % 60).toString().padStart(length = 2, padChar = '0')
+        val minutesText = (seconds / 60).toString().padStart(length = 2, padChar = '0')
+        val secondsText = (seconds % 60).toString().padStart(length = 2, padChar = '0')
 
-        return "$minutes:$seconds"
+        return "$minutesText:$secondsText"
     }
 }
