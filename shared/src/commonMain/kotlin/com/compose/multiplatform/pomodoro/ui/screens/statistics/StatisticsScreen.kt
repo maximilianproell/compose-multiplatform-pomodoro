75847d94637b9e65bb046cf74eb9030f99655db1
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-11-17 11:13:24 +0100

    - Removed unused imports

--- shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/statistics/StatisticsScreen.kt
+++ shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/statistics/StatisticsScreen.kt
@@ -1,10 +1,7 @@
 package com.compose.multiplatform.pomodoro.ui.screens.statistics
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,7 +15,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import at.maximilianproell.multiplatformchart.barchart.BarChart
-import at.maximilianproell.multiplatformchart.barchart.model.BarChartEntry
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
