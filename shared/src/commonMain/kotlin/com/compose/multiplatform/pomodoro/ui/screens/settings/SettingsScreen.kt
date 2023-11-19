12c677c409246420a305779fa84002175692c35d
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-11-19 21:05:12 +0100

    - Added basic text field to SettingsScreen

--- shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/settings/SettingsScreen.kt
+++ shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/settings/SettingsScreen.kt
@@ -1,24 +1,44 @@
 package com.compose.multiplatform.pomodoro.ui.screens.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.compose.multiplatform.pomodoro.MR
+import dev.icerock.moko.resources.compose.stringResource
 
 object SettingsScreen : Screen {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
@@ -27,7 +47,7 @@ object SettingsScreen : Screen {
             topBar = {
                 TopAppBar(
                     title = {
-                        Text("Settings")
+                        Text(stringResource(MR.strings.settings_title))
                     },
                     navigationIcon = {
                         IconButton(onClick = navigator::pop) {
@@ -36,9 +56,40 @@ object SettingsScreen : Screen {
                     }
                 )
             }
-        ) {
-            Column(modifier = Modifier.padding(it)) {
-                Text("This is the settings screen")
+        ) { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues).padding(16.dp)) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = "Pomodoro timer duration in minutes",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                    val keyboardController = LocalSoftwareKeyboardController.current
+                    var mutableText by remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        modifier = Modifier.width(70.dp),
+                        value = mutableText,
+                        onValueChange = {
+                            if (it.length <= 3) {
+                                mutableText = it
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = false,
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(onDone = {
+                            keyboardController?.hide()
+                        }),
+                        singleLine = true,
+                    )
+                }
             }
         }
 
