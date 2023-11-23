32b4f21e2e61f7cb10938977380b79b482877f5f
Maximilian Pröll <maximilian.proell@bitsfabrik.com>
2023-11-23 11:40:13 +0100

    - Removed unused imports in StatisticsScreen.kt

--- shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/statistics/StatisticsScreen.kt
+++ shared/src/commonMain/kotlin/com/compose/multiplatform/pomodoro/ui/screens/statistics/StatisticsScreen.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
